d8fedb1b8345790976ed1082777ec5f47db4d315
Phillip Gajadhar <phillipgajadhar@outlook.com>
2018-03-30 20:22:06 -0400

    Simulate Main fetching user info

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,18 +2,52 @@ import * as React from 'react';
 import { Component } from 'react';
 import Loading from './Loading';
 
+export interface IUserInfo {
+  employeeId: string;
+  role: string;
+  name: string;
+  position: string;
+}
+
 export interface IMainProps {
   token: string;
 }
 
 export interface IMainState {
   loading: boolean;
+  userInfo: IUserInfo;
 }
 
 class Main extends Component<IMainProps, IMainState> {
-  public state = { loading: true };
+  state = {
+    loading: true,
+    userInfo: {
+      employeeId: '',
+      role: '',
+      name: '',
+      position: ''
+    }
+  };
 
-  public render() {
+  private fetchUserInfo = () => {
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+        userInfo: {
+          employeeId: '123456',
+          role: 'Requester',
+          name: 'Rajendra Tiwari',
+          position: 'Supervisor'
+        }
+      });
+    }, 1500);
+  };
+
+  componentDidMount() {
+    this.fetchUserInfo();
+  }
+
+  render() {
     const { loading } = this.state;
 
     if (loading) {
